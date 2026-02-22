6ff22faaafbcc6bdf5817521fa1ffc3ebec70bf2
Kmori04 <kenmori197@gmail.com>
2026-02-22 13:01:39 +0800

    finishing the rooms in floor 2

--- resources/views/floors/2f.blade.php
+++ resources/views/floors/2f.blade.php
@@ -3,53 +3,85 @@
 
 <!-- Floor 2 -->
 
-    <div class="room" style="grid-column: 16 / 24; grid-row: 7 / 13; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 24 / 31; grid-row: 7 / 13; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 31 / 40; grid-row: 7 / 13; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 40 / 46; grid-row: 7 / 13; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 49 / 54; grid-row: 7 / 13; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 54 / 60; grid-row: 7 / 13; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 60 / 66; grid-row: 7 / 13; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 66 / 72; grid-row: 7 / 13; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 75 / 81; grid-row: 5 / 11; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 81 / 89; grid-row: 5 / 11; border: 1.5px solid #6b93a3; background: white;"></div>
+    <!-- TOP PART -->
+    <div class="room" style="grid-column: 14 / 22; grid-row: 8 / 14; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="room" style="grid-column: 22 / 29; grid-row: 8 / 14; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="room" style="grid-column: 29 / 38; grid-row: 8 / 14; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="room" style="grid-column: 38 / 46; grid-row: 8 / 14; border: 1.5px solid #6b93a3; background: white;"></div>
 
-    <div class="room" style="grid-column: 2 / 6; grid-row: 7 / 10; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 5 / 9; grid-row: 11 / 18; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 2 / 7; grid-row: 19 / 26; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 5 / 9; grid-row: 27 / 34; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 5 / 9; grid-row: 34 / 42; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 5 / 9; grid-row: 43 / 50; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 2 / 7; grid-row: 51 / 57; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 2 / 7; grid-row: 58 / 64; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 5 / 9; grid-row: 65 / 72; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 4 / 10; grid-row: 77 / 82; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 5 / 10; grid-row: 83 / 88; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="stairs" style="grid-column: 46 / 51; grid-row: 6 / 14; margin: 1.5px;"></div>
 
-    <div class="room" style="grid-column: 7 / 13; grid-row: 89 / 95; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 13 / 19; grid-row: 89 / 95; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 20 / 26; grid-row: 89 / 95; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 26 / 32; grid-row: 89 / 95; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 32 / 38; grid-row: 89 / 95; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 38 / 44; grid-row: 89 / 95; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 45 / 53; grid-row: 89 / 96; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 54 / 61; grid-row: 89 / 96; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 61 / 68; grid-row: 89 / 96; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 68 / 75; grid-row: 89 / 96; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 75 / 90; grid-row: 89 / 98; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="room" style="grid-column: 51 / 57; grid-row: 8 / 14; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="room" style="grid-column: 57 / 64; grid-row: 8 / 14; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="room" style="grid-column: 64 / 71; grid-row: 8 / 14; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="room" style="grid-column: 71 / 78; grid-row: 8 / 14; border: 1.5px solid #6b93a3; background: white;"></div>
 
-    <div class="room" style="grid-column: 86 / 92; grid-row: 13 / 30; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 84 / 97; grid-row: 30 / 59; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 84 / 88; grid-row: 60 / 78; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 92 / 100; grid-row: 59 / 98; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="stairs" style="grid-column: 78 / 83; grid-row: 6 / 14; margin: 1.5px;"></div>
 
-    <div class="room" style="grid-column: 68 / 74; grid-row: 19 / 32; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 74 / 78; grid-row: 20 / 30; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 68 / 74; grid-row: 33 / 47; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 68 / 73; grid-row: 48 / 62; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 68 / 74; grid-row: 62 / 76; border: 1.5px solid #6b93a3; background: white;"></div>
-    <div class="room" style="grid-column: 74 / 78; grid-row: 63 / 75; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="room" style="grid-column: 83 / 89; grid-row: 6 / 12; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="room" style="grid-column: 89 / 97; grid-row: 6 / 12; border: 1.5px solid #6b93a3; background: white;"></div>
 
-    <div class="room" style="grid-column: 39 / 45; grid-row: 16 / 21; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="room" style="grid-column: 40 / 46; grid-row: 19 / 25; border: 1.5px solid #6b93a3; background: white;"></div>
+
+    <!-- LEFT SIDE 2ND FLOOR -->
+    <div class="stairs stairs-left" style="grid-column: 2 / 9; grid-row: 4 / 10;"></div>
+    <div class="room" style="grid-column: 1 / 7; grid-row: 10 / 16; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="room" style="grid-column: 5 / 9; grid-row: 16 / 25; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="room" style="grid-column: 2 / 7; grid-row: 25 / 33; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="room" style="grid-column: 4 / 10; grid-row: 33 / 40; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="room" style="grid-column: 4 / 10; grid-row: 40 / 47; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="room" style="grid-column: 4 / 10; grid-row: 47 / 54; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="room" style="grid-column: 1 / 7; grid-row: 54 / 60; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="room" style="grid-column: 1 / 7; grid-row: 60 / 66; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="room" style="grid-column: 4 / 10; grid-row: 66 / 72; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="stairs stairs-left" style="grid-column: 4 / 10; grid-row: 72 / 77;"></div>
+    <div class="room" style="grid-column: 4 / 10; grid-row: 77 / 83; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="room" style="grid-column: 4 / 10; grid-row: 83 / 89; border: 1.5px solid #6b93a3; background: white;"></div>
+
+     <!-- Left side ng library -->
+    <div class="room" style="grid-column: 8 / 16;  grid-row: 89 / 95; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="room" style="grid-column: 16 / 24; grid-row: 89 / 95; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="room" style="grid-column: 24 / 32; grid-row: 89 / 95; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="room" style="grid-column: 32 / 40; grid-row: 89 / 95; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="room" style="grid-column: 40 / 48; grid-row: 89 / 95; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="room" style="grid-column: 48 / 56; grid-row: 89 / 95; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="room" style="grid-column: 56 / 66; grid-row: 89 / 96; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="room" style="grid-column: 66 / 74; grid-row: 89 / 96; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="room" style="grid-column: 74 / 82; grid-row: 89 / 96; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="room" style="grid-column: 82 / 90; grid-row: 89 / 96; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="room" style="grid-column: 90 / 106; grid-row: 89 / 98; border: 1.5px solid #6b93a3; background: white;"></div>
+    
+
+
+    <!-- Taas ng Library -->
+    <div class="room" style="grid-column: 96 / 105; grid-row: 12 / 24; border: 1.5px solid #6b93a3; background: white;"></div>
+
+    <!-- below (no hover) -->
+    <div class="room" style="grid-column: 92 / 106; grid-row: 24 / 51; border: 1.5px solid #6b93a3; background: white; pointer-events: none;"></div>
+
+    <!-- ID -->
+    <div class="room" style="grid-column: 92 / 98; grid-row: 51 / 78; border: 1.5px solid #6b93a3; background: white;"></div>
+
+    <!-- stairs on the right side of the room -->
+    <div class="stairs" style="grid-column: 98 / 103; grid-row: 68 / 78; margin: 1.5px;"></div>
+    <!-- Library-->
+    <div class="room" style="grid-column: 106 / 118; grid-row: 24 / 94; border: 1.5px solid #6b93a3; background: white;"></div>
+
+
+
+     <!-- CCS -->
+    <div class="room" style="grid-column: 75 / 81; grid-row: 23 / 36; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="room" style="grid-column: 75 / 81; grid-row: 36 / 49; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="room" style="grid-column: 75 / 81; grid-row: 49 / 62; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="room" style="grid-column: 75 / 81; grid-row: 62 / 75; border: 1.5px solid #6b93a3; background: white;"></div>
+    <div class="stairs stairs-right" style="grid-column: 75 / 81; grid-row: 75 / 82; margin: 1.5px;"></div>
+
+    <!-- CCS CR MEN -->
+    <div class="room" style="grid-column: 81 / 85; grid-row: 23 / 34; border: 1.5px solid #6b93a3; background: white;"></div>
+
+    <!-- CCS CR WOMEN -->
+    <div class="room" style="grid-column: 81 / 85; grid-row: 63 / 75; border: 1.5px solid #6b93a3; background: white;"></div>
+    
+    
 
 </div>
