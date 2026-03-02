879f3ca3cfdba36c81df01d4cf609fa839243fb7
Kmori04 <kenmori197@gmail.com>
2026-03-02 13:15:31 +0800

    3rd floor layout rooms finish

--- resources/views/floors/3f.blade.php
+++ resources/views/floors/3f.blade.php
@@ -1,5 +1,58 @@
-{{-- Floor: 2F --}}
+{{-- Floor: 3F --}}
 {{-- Put ONLY the map blocks here --}}
 
-<!-- Floor 2 -->
+<!-- Floor 3 -->
+
+
+ <!-- TOP LEFT CR -->
  
+<div class="stairs stairs-left" style="grid-column: 2 / 8; grid-row: 8 / 14; margin: 1.5px;"></div>
+<div class="room"   style="grid-column: 2 / 8; grid-row: 14 / 22; margin: 1.5px;"></div>
+
+ <!-- LEFT CBA -->
+<div class="room"   style="grid-column: 5 / 11; grid-row: 22 / 34; margin: 1.5px;"></div>
+<div class="room"   style="grid-column: 5 / 11; grid-row: 34 / 46; margin: 1.5px;"></div>
+<div class="room"   style="grid-column: 5 / 11; grid-row: 46 / 58; margin: 1.5px;"></div>
+<div class="room"   style="grid-column: 5 / 11; grid-row: 58 / 70; margin: 1.5px;"></div>
+<div class="room"   style="grid-column: 5 / 11; grid-row: 70 / 82; margin: 1.5px;"></div>
+<div class="stairs stairs-left" style="grid-column: 5 / 11; grid-row: 82 / 88; margin: 1.5px;"></div>
+
+ <!-- TOP C BUILDING TOP PART -->
+<div class="stairs" style="grid-column: 13 / 17; grid-row: 3 / 13; margin: 1.5px;"></div>
+<div class="room"   style="grid-column: 16 / 25; grid-row: 13 / 20; margin: 1.5px;"></div>
+<div class="room"   style="grid-column: 25 / 34; grid-row: 13 / 20; margin: 1.5px;"></div>
+<div class="room"   style="grid-column: 34 / 43; grid-row: 13 / 20; margin: 1.5px;"></div>
+<div class="room"   style="grid-column: 43 / 52; grid-row: 19 / 26; margin: 1.5px;"></div>
+
+
+ <!-- CCS -->
+<div class="room"   style="grid-column: 81 / 87; grid-row: 26 / 38; margin: 1.5px;"></div>
+<div class="room"   style="grid-column: 81 / 87; grid-row: 38 / 50; margin: 1.5px;"></div>
+<div class="room"   style="grid-column: 81 / 87; grid-row: 50 / 61; margin: 1.5px;"></div>
+<div class="room"   style="grid-column: 81 / 87; grid-row: 61 / 70; margin: 1.5px;"></div>
+<div class="room"   style="grid-column: 81 / 87; grid-row: 70 / 83; margin: 1.5px;"></div>
+<div class="stairs stairs-right" style="grid-column: 82 / 87; grid-row: 83 / 89; margin: 1.5px;"></div>
+
+ <!-- CR CCS -->
+<div class="room"   style="grid-column: 88 / 93; grid-row: 26 / 37; margin: 1.5px;"></div>
+<div class="room" style="grid-column: 88 / 93; grid-row: 78 / 89; margin: 1.5px;"></div>
+
+<!-- LIKOD NG CCS -->
+<div class="room" style="grid-column: 103 / 115; grid-row: 9 / 20; margin: 1.5px;"></div>
+<div class="room" style="grid-column: 103 / 115; grid-row: 9 / 20; margin: 1.5px;"></div>
+<div class="room" style="grid-column: 108 / 114; grid-row: 20 / 34; margin: 1.5px;"></div>
+<div class="room" style="grid-column: 108 / 114; grid-row: 34 / 47; margin: 1.5px;"></div>
+<div class="room" style="grid-column: 108 / 114; grid-row: 47 / 61; margin: 1.5px;"></div>
+<div class="room" style="grid-column: 108 / 114; grid-row: 61 / 75; margin: 1.5px;"></div>
+<div class="room" style="grid-column: 108 / 114; grid-row: 75 / 87; margin: 1.5px;"></div>
+<div class="stairs" style="grid-column: 99 / 103; grid-row: 85 / 94; margin: 1.5px;"></div>
+<div class="room" style="grid-column: 103 / 110; grid-row: 87 / 94; margin: 1.5px;"></div>
+
+
+
+<!-- BACKGROUND -->
+<div class="floor-bg floor3-bg"></div>
+<div class="floor-bg floor3-bg-mid"></div>
+<div class="floor-bg floor3-bg-rightpatch"></div>
+
+
